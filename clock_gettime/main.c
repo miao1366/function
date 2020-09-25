c8531ddea949ad5396fc4a9506db0fe2ac6339bb
miao1366 <1029081208@qq.com>
2020-09-25 13:50:57 +0800

    1.

--- clock_gettime/main.c
+++ clock_gettime/main.c
@@ -1,12 +1,17 @@
 #include <time.h>
 #include <stdio.h>
-
+#include <sys/time.h>
 
 int main() {
 	struct timespec t;
     clock_gettime(CLOCK_MONOTONIC, &t);
-	printf("second:: %ld, nsec:%ld", t.tv_sec, t.tv_nsec);
+	printf("second:: %ld, nsec:%ld\n", t.tv_sec, t.tv_nsec);
 
     clock_gettime(CLOCK_REALTIME, &t);
-	printf("second:: %ld, nsec:%ld", t.tv_sec, t.tv_nsec);
+	printf("second:: %ld, nsec:%ld\n", t.tv_sec, t.tv_nsec);
+
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+
+	printf("second:: %ld, nsec:%lu\n", tv.tv_sec, tv.tv_usec);
 }
