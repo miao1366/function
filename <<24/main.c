4e085e6f24ee79e1def12c3f5febb835e70e2331
miao1366 <1029081208@qq.com>
2020-09-25 13:50:40 +0800

    1.

--- <<24/main.c
+++ <<24/main.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <stdint.h>
 
+
+#define PTYPE_TEXT 0
+#define MESSAGE_TYPE_SHIFT ((sizeof(size_t)-1) * 8)
 #define HANDLE_REMOTE_SHIFT 24
 
 int main() {
@@ -10,5 +13,18 @@ int main() {
 	printf("%u\n", harbor);
 	void *p = (void *)(intptr_t)(~0);
 	printf("%p\n", p);
+
+   
+	int len = 3;
+	size_t sz = len | ((size_t)PTYPE_TEXT << MESSAGE_TYPE_SHIFT);
+	printf("%zu\n", sz);
+  
+	int m = 4;
+    uintptr_t v = (uint32_t)(-m);
+	printf("%lu\n",v);
+	
+	m = 3;
+    v = (uint32_t)(-m);
+	printf("%lu\n",v);
 	return 0;
 }
